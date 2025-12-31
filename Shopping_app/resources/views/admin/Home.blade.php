4c216cb594068d1b9baf979e33abd8f74f936c6b
unknown <Administrator@LAB5-12.labs.org>
2025-12-31 10:58:23 +0500

    Second Commit

---
+++ Shopping_app/resources/views/admin/Home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.adminLayout')
+
+@section('content')
+
+<h1>hello</h1>
+@endsection
--- Shopping_app/routes/web.php
+++ Shopping_app/routes/web.php
@@ -6,6 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view("/home", 'admin.Home');
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
